41c30a1faa1d1411b2f8e247f0ce80d34ba5fa8e
Semyon <bazhanovsemen@gmail.com>
2025-02-07 17:01:59 +0300

    Подправил код

--- C# Block/Calculator/Calculator/Program.cs
+++ C# Block/Calculator/Calculator/Program.cs
@@ -13,18 +13,14 @@ namespace Calculator
             Console.Write("Enter the number a = ");
             if (!Int32.TryParse(Console.ReadLine(), out var a))
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Not a number");
-                Console.ResetColor();
+                PrintError("Not a number");
                 return;
             }
 
             Console.Write("Enter the number b = ");
             if (!Int32.TryParse(Console.ReadLine(), out var b))
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Not a number");
-                Console.ResetColor();
+                PrintError("Not a number");
                 return;
             }
 
@@ -33,39 +29,40 @@ namespace Calculator
             var boolVar = true;
             if (symbol.Length == 0 || symbol.Length > 1 && !boolVar)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Wrong sign!");
-                Console.ResetColor();
+                PrintError("Wrong sign!");
                 return;
             }
-
-            Console.WriteLine();
 
             switch (symbol[0])
             {
                 case '&':
-                    resultInConsole(a, b, '&', a & b);
+                    PrintResult(a, b, '&', a & b);
                     break;
                 case '|':
-                    resultInConsole(a, b, '|', a | b);
+                    PrintResult(a, b, '|', a | b);
                     break;
                 case '^':
-                    resultInConsole(a, b, '^', a ^ b);
+                    PrintResult(a, b, '^', a ^ b);
                     break;
                 default:
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Wrong sign!");
-                    Console.ResetColor();
+                    PrintError("Wrong sign!");
                     break;
             }
         }
 
-        static void resultInConsole(int a, int b, char operation, int result)
+        static void PrintResult(int a, int b, char operation, int result)
         {
+            Console.WriteLine();
             Console.WriteLine($"Result of a {operation} b");
             Console.WriteLine($"Decimal: {Convert.ToString(result, toBase: 10)}");
             Console.WriteLine($"Binary: {Convert.ToString(result, toBase: 2)}");
             Console.WriteLine($"Hexadecimal: {Convert.ToString(result, toBase: 16)}");
+        }
+        static void PrintError(string errror)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(errror);
+            Console.ResetColor();
         }
     }
 }
